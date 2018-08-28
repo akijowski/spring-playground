c1ee581b177dea3ae83d52cdda3f8240fea0588f
Adam Kijowski <adam.kijowski@dish.com>
2018-08-27 18:50:38 -0600

    unit 6 exercise 4 part 2

--- src/test/java/com/dish/springplayground/services/WordCounterTest.java
+++ src/test/java/com/dish/springplayground/services/WordCounterTest.java
@@ -1,7 +1,5 @@
 package com.dish.springplayground.services;
 
-import com.dish.springplayground.config.WordCounterConfig;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
